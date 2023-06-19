c3a15d828fa520dfa3b42cb34de010df8295852e
Marcus Ang <marcusajh0802@gmail.com>
2023-06-20 06:35:20 +0800

    add search

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -3,6 +3,11 @@ import markdown2
 
 from . import util
 
+def index(request):
+    return render(request, 'encyclopedia/index.html', {
+        "entries": util.list_entries
+    })
+
 def convert_md_to_html(title):
     content = util.get_entry(title)
     markdowner = markdown2.Markdown()
@@ -10,12 +15,6 @@ def convert_md_to_html(title):
         return None
     else:
         return markdowner.convert(content)
-
-
-def index(request):
-    return render(request, 'encyclopedia/index.html', {
-        "entries": util.list_entries
-    })
 
 def entry(request, title):
     html_content = convert_md_to_html(title)
@@ -29,3 +28,21 @@ def entry(request, title):
             "content": html_content
         })
 
+def search(request): #request with "POST" method is passed in here, default method for a request is "GET", POST = submit data 
+    if request.method == "POST":
+        entry_search = request.POST.get('q')
+        html_content = convert_md_to_html(entry_search)
+        if html_content is not None:
+            return render(request, 'encyclopedia/entry.html', {
+                "title": entry_search,
+                "content": html_content
+            })
+        else:
+            all_entries = util.list_entries()
+            recommendation = []
+            for entry in all_entries:
+                if entry_search.lower() in entry.lower():
+                    recommendation.append(entry)
+            return render(request, "search.html", {
+                "recommendation": recommendation
+            })

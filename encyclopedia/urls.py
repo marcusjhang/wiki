1ae273a8365de5a6cec93a82c404c26c41a08b88
Marcus Ang <marcusajh0802@gmail.com>
2023-06-20 08:03:25 +0800

    add functionality to search bar

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry, name="entry"),
-    path("search/", views.search, name="search"),
+    path("wiki/<str:title>", views.entry, name="entry"), #a request is sent to views.entry, default method is "GET", title is the input
+    path("search/", views.search, name="search"), #request is sent to views.search
+    path("new/", views.new_page, name="new_page"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -16,7 +16,7 @@ def convert_md_to_html(title):
     else:
         return markdowner.convert(content)
 
-def entry(request, title):
+def entry(request, title): #takes request and title as input, returns a rendered HTML file as the response
     html_content = convert_md_to_html(title)
     if html_content == None:
         return render(request, 'encyclopedia/error.html', {
@@ -28,12 +28,12 @@ def entry(request, title):
             "content": html_content
         })
 
-def search(request): #request with "POST" method is passed in here, default method for a request is "GET", POST = submit data 
+def search(request): #request is of method "POST", returns a rendered form of a html file as the response
     if request.method == "POST":
         entry_search = request.POST['q']
         html_content = convert_md_to_html(entry_search)
         if html_content is not None:
-            return render(request, 'encyclopedia/entry.html', {
+            return render(request, 'encyclopedia/entry.html', { 
                 "title": entry_search,
                 "content": html_content
             })
@@ -43,6 +43,25 @@ def search(request): #request with "POST" method is passed in here, default meth
             for entry in all_entries:
                 if entry_search.lower() in entry.lower():
                     recommendation.append(entry)
-            return render(request, "encyclopedia/search.html", {
+            return render(request, 'encyclopedia/search.html', {
                 "recommendation": recommendation
+            })
+        
+def new_page(request):
+    if request.method == "GET":
+        return render(request, "encyclopedia/new.html")
+    else:
+        title = request.POST['title']
+        content = request.POST['content']
+        titleExist = util.get_entry(title)
+        if titleExist is not None:
+            return render(request, 'encyclopedia/error.html', {
+                "message": "Entry page already exists"
+            })
+        else:
+            util.save_entry(title, content)
+            html_content = convert_md_to_html(content)
+            return render(request, "encyclopedia/entry.html", {
+                "title" : title,
+                "content" : html_content
             })
